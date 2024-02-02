260046ac5e1c90949f2db2755a7f929011e3bd3b
SnorlaxMonster <snorlaxmonster@users.noreply.github.com>
2024-02-02 18:17:45 +1100

    Split web requests to scrapewiki.py
    To facilitate reusing many of the API requests for other purposes,
    I have split the script across two Python files. addredirect.py now
    only contains code related to adding new redirect entries,
    while scrapewiki.py contains code related to web scraping and API
    requests.

--- addredirect.py
+++ addredirect.py
@@ -4,16 +4,19 @@ Python script for adding a new wiki redirect
 import re
 import json
 import os
-from urllib.parse import urlparse, urlunparse, urljoin, ParseResult as UrlParseResult
+from urllib.parse import urlparse
 import requests
-import lxml.html
 import unicodedata
 from io import BytesIO
-from typing import Optional
+from typing import Optional, Iterable
 from PIL import Image
+
+from scrapewiki import normalize_url_protocol, normalize_wikia_url, get_mediawiki_api_url, get_favicon_url, \
+    determine_wiki_software, request_with_error_handling, request_siteinfo
 
 NO_VALUES = {"", "n", "no"}
 YES_VALUES = {"y", "yes"}
+
 
 def extract_hostname(url: str) -> str:
     """
@@ -24,279 +27,6 @@ def extract_hostname(url: str) -> str:
     """
     parsed_url = urlparse(url)
     return parsed_url.hostname
-
-
-def normalize_url(url: str, default_protocol="https") -> str:
-    """
-    Enforces that the URL specifies a protocol.
-
-    :param url: Unnormalized URL
-    :param default_protocol: Protocol to use to access the URL, if one is not specified
-    :return: URL with a protocol
-    """
-    if url.startswith(("http://", "https://")):
-        return url
-    elif url.startswith("//"):
-        return f"{default_protocol}:{url}"
-    else:
-        return f"{default_protocol}://{url}"
-
-
-def normalize_wikia_url(original_url: str) -> str:
-    """
-    Old Wikia URLs included the language as a subdomain for non-English wikis, but these URLs no longer work.
-    Non-English Wikia URLs need to be modified to move the language to the path of the URL.
-
-    :param original_url: Wikia URL
-    :return: Modified URL with language moved to the path, if necessary
-    """
-    # Ignore non-Wikia URLs
-    if "wikia.com" not in original_url:
-        return original_url
-
-    # Parse the URL
-    parsed_url = urlparse(normalize_url(original_url))
-
-    # Extract the URL components
-    lang_match = re.match(r"([a-z]+)\.(.*)\.wikia\.com", parsed_url.hostname)
-    if not lang_match:
-        return original_url
-    lang, subdomain = lang_match.groups()
-
-    # Construct the new URL
-    new_domain = f"{subdomain}.fandom.com"  # Always use "fandom.com" when restructuring Wikia URLs
-    new_path = urljoin(lang, parsed_url.path)
-    parsed_url = parsed_url._replace(netloc=new_domain, path=new_path)
-
-    return urlunparse(parsed_url)
-
-
-def normalize_relative_url(parsed_relative_url: UrlParseResult, parsed_absolute_url: UrlParseResult) -> str:
-    """
-    Ensures that a URL includes the protocol and domain name, and does not include a query.
-    For example, if the input URL is "/w/api.php?action=rsd", adds the protocol and domain name to the URL,
-    and deletes action=rsd.
-
-    :param parsed_relative_url: Pre-parsed URL to be normalized
-    :param parsed_absolute_url: Pre-parsed URL to use to fill in gaps in the first URL
-    :return: Normalized API URL
-    """
-    parsed_new_url = parsed_relative_url
-    if parsed_new_url.netloc == "":
-        parsed_new_url = parsed_new_url._replace(netloc=parsed_absolute_url.netloc)
-    if parsed_new_url.scheme == "":
-        parsed_new_url = parsed_new_url._replace(scheme=parsed_absolute_url.scheme)
-    if parsed_new_url.query != "":
-        parsed_new_url = parsed_new_url._replace(query="")
-
-    return urlunparse(parsed_new_url)
-
-
-def request_with_error_handling(raw_url: str, ignorable_errors: Optional[list[int]] = None,
-                                **kwargs) -> Optional[requests.Response]:
-    """
-    Given a base URL, attempts to resolve the URL as HTTPS, then falls back to HTTP if that isn't possible.
-
-    :param raw_url: URL to resolve
-    :param ignorable_errors: Error codes that should be ignored as long as the response is not null
-    :param kwargs: kwargs to use for the HTTP/HTTPS requests
-    :return: GET request response
-    """
-    ignorable_errors = [] if (ignorable_errors is None) else ignorable_errors
-    url = normalize_url(raw_url)
-
-    # GET request the URL
-    try:
-        response = requests.get(url, **kwargs)
-
-    # If using HTTPS results in an SSLError, try HTTP instead
-    except requests.exceptions.SSLError:
-        parsed_url = urlparse(url)
-        if parsed_url.scheme != "http":
-            print(f"⚠ SSLError. Defaulting to HTTP connection for {raw_url}")
-            url = urlunparse(parsed_url._replace(scheme="http"))
-            return request_with_error_handling(url, ignorable_errors=ignorable_errors, **kwargs)
-        else:
-            print(f"⚠ SSLError: Unable to connect to {raw_url} , even via HTTP")
-            return None
-
-    # If unable to connect to the URL at all, return None
-    except requests.exceptions.ConnectionError:
-        print(f"⚠ ConnectionError: Unable to connect to {url}")
-        return None
-
-    # If the response is an error, handle the error
-    if not response:
-        # If the error is in the set of acceptable errors and the response included any content, ignore the error
-        if response.status_code in ignorable_errors and response.content:
-            pass
-
-        # For Error 404 and 410, the wiki presumably does not exist, so abort
-        # This could mean that only the Main Page does not exist (while the wiki does), but that case is ignored
-        if response.status_code in (404, 410):
-            print(f"⚠ Error {response.status_code} returned by {url} . Page does not exist. Aborting.")
-            return None
-
-        # For other errors, they can usually be fixed by adding a user-agent
-        else:
-            print(f"⚠ Error {response.status_code} returned by {url} . Aborting.")
-            return None
-
-    # Otherwise, return the response
-    return response
-
-
-def is_mediawiki(parsed_html: lxml.html.etree) -> bool:
-    """
-    Checks if the page is a MediaWiki page.
-    MediaWiki pages can be identified by containing "mediawiki" as a class on the <body> element.
-
-    :param parsed_html: LXML etree representation of the page's HTML
-    :return: Whether the page is a page from a MediaWiki site
-    """
-    body_elem = parsed_html.find('body')
-    if body_elem is None:
-        return False
-
-    body_class = body_elem.get('class')
-    if body_class is None:
-        return False
-
-    if 'mediawiki' in body_class.split():
-        return True
-    else:
-        return False
-
-
-def determine_wiki_software(response: Optional[requests.Response]) -> Optional[str]:
-    if not response:
-        return None
-
-    # Parse the HTML
-    parsed_html = lxml.html.parse(BytesIO(response.content))
-
-    # Check the wiki's software
-    if is_mediawiki(parsed_html):
-        return "mediawiki"
-    else:
-        return None  # unable to determine the wiki's software
-
-
-def get_favicon_url(response: Optional[requests.Response]) -> Optional[str]:
-    if not response:
-        return None
-
-    parsed_html = lxml.html.parse(BytesIO(response.content))
-
-    # Find the icon element in the HTML
-    icon_link_element = parsed_html.find('//link[@rel="shortcut icon"]')
-    if icon_link_element is None:
-        icon_link_element = parsed_html.find('//link[@rel="icon"]')
-    if icon_link_element is None:
-        return None
-
-    # Retrieve the URL from the icon element
-    icon_url = icon_link_element.get("href")
-    if icon_url is None:
-        return None
-
-    return normalize_relative_url(urlparse(icon_url), urlparse(response.url))
-
-
-def get_api_url(response: requests.Response) -> Optional[str]:
-    """
-    Given an HTTP/HTTPS response for a wiki page, determines the wiki's API URL.
-
-    :param response: HTTP/HTTPS response for a wiki page
-    :return: Wiki's API URL
-    """
-    # Parse the HTML
-    parsed_html = lxml.html.parse(BytesIO(response.content))
-
-    # If the site is not a MediaWiki wiki, abort trying to determine the API URL
-    if not is_mediawiki(parsed_html):
-        print(f"⚠ {response.url} is not a MediaWiki page")
-        return None
-
-    # Retrieve the API URL via EditURI element
-    edit_uri_node = parsed_html.find('/head/link[@rel="EditURI"]')
-    if edit_uri_node is not None:
-        parsed_api_url = urlparse(edit_uri_node.get('href'))
-        parsed_response_url = urlparse(response.url)
-
-        return normalize_relative_url(parsed_api_url, parsed_response_url)
-
-    # If EditURI is missing, try to find the permalink URL and determine the API URL from that
-    permalink_node = parsed_html.find('//li[@id="t-permalink"]/a')
-    if permalink_node is not None:
-        print(f"ℹ Retrieved API URL for {response.url} via permalink node")
-        permalink_url = permalink_node.get("href")
-        parsed_permalink_url = urlparse(permalink_url)
-        parsed_api_url = parsed_permalink_url._replace(path=parsed_permalink_url.path.replace('index.php', 'api.php'))
-        parsed_response_url = urlparse(response.url)
-
-        return normalize_relative_url(parsed_api_url, parsed_response_url)
-
-    # Otherwise, the API URL retrieval has failed
-    print(f"⚠ Unable to determine API URL for {response.url}")
-    return None
-
-
-def get_api_url_via_request(wiki_url: str, headers: Optional[dict] = None) -> Optional[str]:
-    """
-    Given a wiki's URL, determines what its API URL is.
-
-    :param wiki_url: Wiki's URL
-    :param headers: Headers to include in the request (e.g. user-agent)
-    :return: Wiki's API URL
-    """
-    response = request_with_error_handling(wiki_url, headers=headers)
-    if response is None:
-        return None
-
-    return get_api_url(response)
-
-
-def query_wiki_api(api_url: str, query_params: dict, headers: Optional[dict] = None) -> Optional[dict]:
-    """
-    Runs a MediaWiki API query with the specified parameters.
-
-    :param api_url:
-    :param query_params: Query parameters to use
-    :param headers: Headers to include in the request (e.g. user-agent)
-    :return: API query result
-    """
-    # GET request API query
-    response = request_with_error_handling(api_url, params=query_params, headers=headers)
-
-    # If the response is an error, do not attempt to parse it as JSON
-    if not response:
-        print(f"Error {response.status_code} from {api_url}")
-        return None
-
-    # Parse as JSON
-    query_json = response.json()
-
-    # If the response contains an error, do not attempt the retrieve the content
-    if query_json.get("error"):
-        print(f"Error {query_json['error'].get('code')} from {api_url}")
-        return None
-
-    return query_json["query"]
-
-
-def request_siteinfo(api_url: str, headers: Optional[dict] = None) -> Optional[dict]:
-    """
-    Runs a "siteinfo" query using the MediaWiki API of the specified wiki, for 'general' data.
-
-    :param api_url: MediaWiki API URL
-    :param headers: Headers to include in the request (e.g. user-agent).
-    :return: siteinfo query result
-    """
-    siteinfo_params = {'action': 'query', 'meta': 'siteinfo', 'siprop': 'general', 'format': 'json'}
-    siteinfo = query_wiki_api(api_url, siteinfo_params, headers=headers)
-
-    return siteinfo
 
 
 def extract_site_metadata_from_siteinfo(siteinfo: dict) -> dict:
@@ -319,7 +49,7 @@ def extract_site_metadata_from_siteinfo(siteinfo: dict) -> dict:
 
     # Detect if the wiki is on a wikifarm
     logo_path = siteinfo_general.get("logo", "")  # Not guaranteed to be present
-    wikifarm = get_wikifarm(base_url, logo_path)
+    wikifarm = detect_wikifarm([base_url, logo_path])
 
     # Apply standard wiki name changes
     if ".fandom.com" in base_url:
@@ -350,7 +80,7 @@ def extract_site_metadata_from_siteinfo(siteinfo: dict) -> dict:
 
 def extract_topic_from_url(wiki_url: str) -> str:
     # Retrieve the topic
-    normalized_url = normalize_url(wiki_url)
+    normalized_url = normalize_url_protocol(wiki_url)
     hostname = extract_hostname(normalized_url)
     domain_parts = hostname.split('.')
     topic = domain_parts[0].replace("-", "")
@@ -399,7 +129,7 @@ def validate_wiki_languages(origin_site_metadata: dict, destination_site_metadat
     return True
 
 
-def get_wikifarm(base_url: str, logo_url: str = "") -> Optional[str]:
+def detect_wikifarm(url_list: Iterable[str]) -> Optional[str]:
     # If the site URL or logo URL contains the name of a wikifarm, assume the wiki is hosted on that wikifarm
     # Checking the logo URL should catch any wikis hosted on a wikifarm that use a custom URL
 
@@ -407,8 +137,9 @@ def get_wikifarm(base_url: str, logo_url: str = "") -> Optional[str]:
     known_wikifarms = {"shoutwiki", "wiki.gg", "miraheze", "wikitide"}
 
     for wikifarm in known_wikifarms:
-        if wikifarm in base_url or wikifarm in logo_url:
-            return wikifarm
+        for url in url_list:
+            if wikifarm in url:
+                return wikifarm
     return None
 
 
@@ -498,7 +229,7 @@ def download_wiki_icon(icon_url: str, wiki_name: str, language_code: str,
     """
     # Download icon file
     try:
-        icon_file_response = requests.get(normalize_url(icon_url), headers=headers)
+        icon_file_response = requests.get(normalize_url_protocol(icon_url), headers=headers)
     except requests.exceptions.ConnectionError:
         icon_file_response = None
 
@@ -674,12 +405,16 @@ def add_redirect_entry_from_url(origin_wiki_url: str, destination_wiki_url: str,
     origin_wiki_url = normalize_wikia_url(origin_wiki_url)
 
     # Determine the API URLs
-    origin_api_url = get_api_url_via_request(origin_wiki_url, headers=headers)
-    destination_api_url = get_api_url_via_request(destination_wiki_url, headers=headers)
+    origin_api_url = get_mediawiki_api_url(origin_wiki_url, headers=headers)
+    destination_api_url = get_mediawiki_api_url(destination_wiki_url, headers=headers)
 
     # Validate the API URLs
-    if origin_api_url is None or destination_api_url is None:
-        print(f"⚠ Unable to add redirection from {origin_wiki_url} to {destination_wiki_url}.")
+    if origin_api_url is None:
+        print(f"⚠ Unable to determine API URL for {origin_wiki_url}")
+        return None
+
+    if destination_api_url is None:
+        print(f"⚠ Unable to determine API URL for {destination_wiki_url}")
         return None
 
     # Request siteinfo data
@@ -759,7 +494,7 @@ def get_wiki_metadata_cli(site_class, key_properties, headers: Optional[dict] = 
     # For MediaWiki wikis, retrieve details via the API
     if wiki_software == "mediawiki":
         print(f"🕑 Getting {site_class} site info...")
-        api_url = get_api_url(response)
+        api_url = get_mediawiki_api_url(response)
         if api_url is None:
             print(f"⚠ Unable to automatically retrieve API URL for {wiki_url}.")
             api_url = input(f"📥 Enter {site_class} wiki API URL: ")
@@ -770,7 +505,9 @@ def get_wiki_metadata_cli(site_class, key_properties, headers: Optional[dict] = 
             wiki_data = extract_site_metadata_from_siteinfo(siteinfo)
         else:
             print(f"⚠ Unable to retrieve metadata via MediaWiki API")
-
+    else:
+        print(f"⚠ {wiki_url} uses currently unsupported software. Details will need to be entered manually.")
+    
     # If the icon URL was not found via the API, try to find it from the HTML
     if "icon path" in key_properties:
         icon_path = wiki_data.get("icon path")
