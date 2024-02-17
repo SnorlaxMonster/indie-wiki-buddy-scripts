060d888d1658e0b0ab84808c3158e0209802135f
SnorlaxMonster <snorlaxmonster@users.noreply.github.com>
2024-02-17 22:16:30 +1100

    Support wiki comparisons

---
+++ compare.py
@@ -0,0 +1,41 @@
+import pandas as pd
+
+from utils import normalize_url_protocol
+from profilewiki import profile_wiki
+
+# Ask how many wikis to compare
+wiki_count_input = "null"
+while not (wiki_count_input.isnumeric() or wiki_count_input.strip() == ""):
+    wiki_count_input = input("How many wikis do you want to compare (default: 2)?: ")
+if wiki_count_input.strip() == "":
+    wiki_count_input = 2
+
+# Take URLs as input
+input_wiki_urls = []
+for wiki_index in range(int(wiki_count_input)):
+    wiki_url = ""
+    while wiki_url.strip() == "":
+        wiki_url = input(f"📥 Enter wiki URL {wiki_index+1}: ").strip()
+    wiki_url = normalize_url_protocol(wiki_url)
+    input_wiki_urls.append(wiki_url)
+
+# Profile each wiki
+all_profiles = []
+for wiki_url in input_wiki_urls:
+    print(f"🕑 Profiling {wiki_url}")
+    wiki_profile = profile_wiki(wiki_url)
+    all_profiles.append(wiki_profile)
+
+# Create output DataFrame
+df = pd.DataFrame(all_profiles)
+
+output_df = df[["name", "content_pages", "active_users", "recent_edit_count"]]
+output_df = output_df.rename(columns={"content_pages": "Content pages", "active_users": "Active users",
+                                      "recent_edit_count": "Recent edits (last 30 days)"})
+output_df = output_df.set_index("name")
+output_df = output_df.transpose()
+output_df.columns.name = None
+output_df.index.name = "Metric"
+
+# Print Markdown table
+print(output_df.to_markdown())
